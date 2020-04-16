f74c647893943713b173dcbb3120917e8b00e849
Miguel Angel <miguelapb35@gmail.com>
2020-04-15 19:53:57 -0500

    Updates

---
+++ precache-manifest.fe2db92071b9e8ed962f470d3a93d1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974e0fd95f51e4096a34cded200a24ee",
+    "url": "/react-useState-hook/index.html"
+  },
+  {
+    "revision": "c06308d47f78b0181de1",
+    "url": "/react-useState-hook/static/css/main.ba95991a.chunk.css"
+  },
+  {
+    "revision": "6a7bfd91fd16282762bf",
+    "url": "/react-useState-hook/static/js/2.b72fd079.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-useState-hook/static/js/2.b72fd079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06308d47f78b0181de1",
+    "url": "/react-useState-hook/static/js/main.3d510833.chunk.js"
+  },
+  {
+    "revision": "b66e7e0c623206b42fa0",
+    "url": "/react-useState-hook/static/js/runtime-main.82163462.js"
+  }
+]);
